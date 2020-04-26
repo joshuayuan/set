c3295cec8c80f6e1e316fc163730ec757223da46
Joshua Yuan <joshua43210@gmail.com>
2020-04-26 02:31:54 -0700

    Added button to clear and reset selected cards. Not sure what happens if we run out of cards though.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,24 @@ class Card extends React.Component {
   }
 }
 
+class Board extends React.Component {
+  render() {
+    const res = [];
+    for (let i = 0; i < this.props.cards.length; i++) {
+      res.push(
+        <Card
+          key={i}
+          value={this.props.cards[i]}
+          clickHandler={() => this.props.onClickCard(i)}
+          isSelected={this.props.selected[i]}
+        />
+      );
+    }
+
+    return res;
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -24,35 +42,37 @@ class App extends React.Component {
     };
   }
 
-  renderBoard() {
-    const res = [];
-    for (let i = 0; i < this.state.cards.length; i++) {
-      res.push(
-        <Card
-          key={i}
-          value={this.state.cards[i]}
-          clickHandler={() => this.onClickCard(i)}
-          isSelected={this.state.selected[i]}
-        />
-      );
-    }
-
-    return res;
-  }
-
   onClickCard(position) {
     const selected = this.state.selected.slice();
     selected[position] = !selected[position];
     this.setState({selected: selected});
   }
 
+  onClickEnter() {
+    const newCards = this.state.cards.slice();
+    const selectedIndexes = [];
+    for (let i = 0; i < this.state.selected.length; i++) {
+      if (this.state.selected[i]) selectedIndexes.push(i);
+    }
+    console.log(selectedIndexes);
+    const [drawnCards, newDeck] = drawNCardsFromDeck(selectedIndexes.length, this.state.deck);
+    for (let j = 0; j < selectedIndexes.length; j++) {
+      newCards[selectedIndexes[j]] = drawnCards[j];
+    }
+    this.setState({cards: newCards, selected: Array(12).fill(false), deck: newDeck});
+  }
+
   render() {
-    const board = this.renderBoard();
     return (
       <div className="App">
         <div className="Board">
-          {board}
+          <Board
+            cards={this.state.cards}
+            onClickCard={(i) => this.onClickCard(i)}
+            selected={this.state.selected}
+          />
         </div>
+        <button className="Button" onClick={() => this.onClickEnter()}>Enter</button>
       </div>
     );
   }
