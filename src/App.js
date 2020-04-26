192e6c7cccff582d8c0dd9194ab98a2d9412dd61
Joshua Yuan <joshua43210@gmail.com>
2020-04-25 23:12:16 -0700

    Created a board of 12 grid items representing cards with hover and select effect Right now they all just have value of "0".
    Future short term plans:
    - fix the graphics / UI
    - render the set cards
    - game logic

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component {
+  render() {
+    const classes = "Card" + (this.props.isSelected ? " Selected" : "");
+    console.log(classes);
+    return (
+      <div className={classes} onClick={this.props.clickHandler}>
+        <h1>{this.props.value}</h1>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: Array(12).fill(0),
+      selected: Array(12).fill(false),
+    };
+  }
+
+  renderBoard() {
+    const res = [];
+    for (let i = 0; i < this.state.cards.length; i++) {
+      res.push(
+        <Card
+          key={i}
+          value={this.state.cards[i]}
+          clickHandler={() => this.onClickCard(i)}
+          isSelected={this.state.selected[i]}
+        />
+      );
+    }
+
+    return res;
+  }
+
+  onClickCard(position) {
+    const selected = this.state.selected.slice();
+    selected[position] = !selected[position];
+    this.setState({selected: selected});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Board">
+          {this.renderBoard()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
